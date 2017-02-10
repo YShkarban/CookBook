b48112609493c746691d0c5aef2f6db7072e5d6f
Rostyslav Kurdiumov <Rostyslav Kurdiumov>
2017-02-10 22:20:37 +0100

    Initial commit

---
+++ CoreUnitTest/CoreUnitTest.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace CoreUnitTest
+{
+    [TestClass]
+    public class CoreUnitTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
