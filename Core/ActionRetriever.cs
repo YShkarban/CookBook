ce0c0884e77e6994ea085ff7ba0689162a84b752
Kurdiumov <rostyslav.kurdiumov@gmail.com>
2017-02-14 23:39:26 +0100

    Public interface for accesing objects implemented.

---
+++ Core/ActionRetriever.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Remoting.Channels;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core
+{
+    public static class ActionRetriever
+    {
+
+        public static object SendAction(string action, string targetObject, string id,  object args, object sender)
+        {
+            IAction actionToExecute;
+            object result = null;
+
+            switch (targetObject)
+            {
+                case "User":
+                    actionToExecute = new IActionUser();
+                    break;
+                case "Recipe":
+                    actionToExecute = new IActionRecipe();
+                    break;
+                case "CookBook":
+                    actionToExecute = new IActionCookBook();
+                    break;
+                default:
+                    throw new ArgumentException();
+            }
+
+            switch (action)
+            {
+                case "Get":
+                    result = actionToExecute.Get();
+                    break;
+                case "Remove":
+                    result = actionToExecute.Remove();
+                    break;
+                case "Add":
+                case "Create":
+                    result = actionToExecute.Create();
+                    break;
+                case "Update":
+                    result = actionToExecute.Update();
+                    break;
+                default:
+                    throw new ArgumentException();
+            }
+
+            return result;
+        }
+    }
+}
---
+++ Core/IActionCookBook.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core
+{
+    class IActionCookBook: IAction
+    {
+        public object Create()
+        {
+            throw new NotImplementedException();
+        }
+
+        public object Remove()
+        {
+            throw new NotImplementedException();
+        }
+
+        public object Get()
+        {
+            throw new NotImplementedException();
+        }
+
+        public object Update()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
